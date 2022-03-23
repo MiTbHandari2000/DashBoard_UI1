053d6cefcf850545b5fe79bfc73d8bbc78c5f1be
meet.bhandari <meet.bhandari@1R-74>
2022-03-23 16:21:33 +0530

    Recycler view added for wishlist

---
+++ Adapters/AdapterForWishlist.cs
@@ -0,0 +1,88 @@
+﻿using AndroidX.RecyclerView.Widget;
+using Android.Views;
+using Android.Widget;
+using DashBoard_UI1.DataProvider;
+using System;
+using System.Collections.Generic;
+using Android.Content;
+using AndroidX.CardView.Widget;
+
+namespace DashBoard_UI1.Adapters
+{
+    public class AdapterForWishlist : RecyclerView.Adapter
+    {
+        public event EventHandler<AdapterForWishlistClickEventArgs> ItemClick;
+        public event EventHandler<AdapterForWishlistClickEventArgs> ItemLongClick;
+        List<DataProviderClass> items;
+        Context cont;
+
+        public AdapterForWishlist(List<DataProviderClass> mydataproviderlistIn, Context con)
+        {
+            this.items = mydataproviderlistIn;
+            this.cont = con;
+        }
+
+        // Create new views (invoked by the layout manager)
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+
+            //Setup your layout here
+            View itemView = null;
+            var id = Resource.Layout.rowitem;
+            itemView = LayoutInflater.From(parent.Context).Inflate(id, parent, false);
+
+            var vh = new AdapterForWishlistViewHolder(itemView, OnClick, OnLongClick);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            var item = items[position];
+
+            // Replace the contents of the view with that element
+            var holder = viewHolder as AdapterForWishlistViewHolder;
+            //holder.TextView.Text = items[position];
+            holder.thumbnail.Text = item.name;
+            holder.image.SetImageResource(item.image);
+            if (position % 2 == 0)
+            {
+                holder.cardview.SetBackgroundDrawable(cont.GetDrawable(Resource.Drawable.background1));
+            }
+            else
+            {
+                holder.cardview.SetBackgroundDrawable(cont.GetDrawable(Resource.Drawable.background2));
+            }
+        }
+
+        public override int ItemCount => items.Count;
+
+        void OnClick(AdapterForWishlistClickEventArgs args) => ItemClick?.Invoke(this, args);
+        void OnLongClick(AdapterForWishlistClickEventArgs args) => ItemLongClick?.Invoke(this, args);
+
+    }
+
+    public class AdapterForWishlistViewHolder : RecyclerView.ViewHolder
+    {
+        public TextView thumbnail;
+        public ImageView image;
+        public CardView cardview;
+
+        public AdapterForWishlistViewHolder(View itemView, Action<AdapterForWishlistClickEventArgs> clickListener,
+                            Action<AdapterForWishlistClickEventArgs> longClickListener) : base(itemView)
+        {
+            //TextView = v;
+            cardview = itemView.FindViewById<CardView>(Resource.Id.cardimage0);
+            thumbnail = itemView.FindViewById<TextView>(Resource.Id.rowitemtextview);
+            image = itemView.FindViewById<ImageView>(Resource.Id.rowitemimageView);
+            itemView.Click += (sender, e) => clickListener(new AdapterForWishlistClickEventArgs { View = itemView, Position = AdapterPosition });
+            itemView.LongClick += (sender, e) => longClickListener(new AdapterForWishlistClickEventArgs { View = itemView, Position = AdapterPosition });
+        }
+    }
+
+    public class AdapterForWishlistClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -2,24 +2,53 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using AndroidX.RecyclerView.Widget;
+using DashBoard_UI1.Adapters;
+using DashBoard_UI1.DataProvider;
+using System;
+using System.Collections.Generic;
+
+using static AndroidX.RecyclerView.Widget.RecyclerView;
 
 namespace DashBoard_UI1
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        public RecyclerView myrecycler;
+        public AdapterForWishlist myadapter;
+        public List<DataProviderClass> mydataproviderlist;
+        public LinearLayoutManager layoutmanager;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            myrecycler = FindViewById<RecyclerView>(Resource.Id.recyclerViewforwishlist);
+            inputData();
+            myadapter = new AdapterForWishlist(mydataproviderlist, this);
+            layoutmanager = new LinearLayoutManager(this, LinearLayoutManager.Horizontal, false);
+            myrecycler.SetLayoutManager(layoutmanager);
+            myrecycler.SetAdapter(myadapter);
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        private void inputData()
+        {
+            mydataproviderlist = new List<DataProviderClass>
+            {
+                new DataProviderClass{name ="Tee",image = Resource.Drawable.clothes},
+                new DataProviderClass{name ="Gym",image = Resource.Drawable.ic_dumbell},
+                new DataProviderClass{name ="Bike",image = Resource.Drawable.ic_bike},
+                new DataProviderClass{name ="Savings",image = Resource.Drawable.bitcoin},
+                new DataProviderClass{name ="Tee",image = Resource.Drawable.clothes},
+                new DataProviderClass{name ="Gym",image = Resource.Drawable.ic_dumbell},
+                new DataProviderClass{name ="Bike",image = Resource.Drawable.ic_bike},
+                new DataProviderClass{name ="Tee",image = Resource.Drawable.clothes},
+                new DataProviderClass{name ="Gym",image = Resource.Drawable.ic_dumbell},
+                new DataProviderClass{name ="Bike",image = Resource.Drawable.ic_bike}
+            };
         }
     }
 }
